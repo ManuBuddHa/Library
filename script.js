72227a588479872baf756ecbda9614637d620707
Manu BuddHa <ferano915@gmail.com>
2025-03-09 17:47:08 +0530

    Need to add Read function

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
 const myLibrary = [];
-const dialog=document.querySelector("#openForm")
-function book(bookName,bookAuthor,bookPages,bookID){
-   this.bookName = bookName;
-   this.bookAuthor = bookAuthor;
-   this.bookPages = bookPages;
-   this.bookID = bookID;
+const dialog=document.querySelector("#openForm");
+const library=document.querySelector(".library");
+function book(Name,Author,Pages,ID){
+   this.bookName = Name;
+   this.bookAuthor = Author;
+   this.bookPages = Pages;
+   this.bookID = ID;
 }
 function addBook(){
-   preventDefault();
-   const bookName=document.querySelector("#bookName");
-   const bookAuthor=document.querySelector("#bookAuthor");
-   const bookPages=document.querySelector("#bookPages");
-   const bookID=crypto.randomUUID();
-   const newBook=new book(bookName.value,bookAuthor.value,bookPages.value,bookID);
+   event.preventDefault();
+   const Name=document.querySelector("#bookName");
+   const Author=document.querySelector("#bookAuthor");
+   const Pages=document.querySelector("#bookPages");
+   const ID=crypto.randomUUID();
+   const newBook=new book(Name.value,Author.value,Pages.value,ID);
    myLibrary.push(newBook);
-   bookName.innerHTML='';
-   bookAuthor.innerHTML='';
-   bookPages.innerHTML='';
+   Name.value="";
+   Author.value="";
+   Pages.value="";
+   closeForm();
+   display();
 }
 function openForm(){
    dialog.showModal();
 }
 function closeForm(){
+   event.preventDefault();
    dialog.close();
-   console.log(myLibrary)
+}
+function display(){
+   library.innerHTML="";
+   myLibrary.forEach(books=>{
+      const row=document.createElement("div");
+      row.style.backgroundColor="pink";
+      row.style.padding="1vh";
+      const heading=document.createElement("h2");
+      heading.innerText=`${books.bookName} by ${books.bookAuthor} - ${books.bookPages}`;
+      row.appendChild(heading);
+      library.appendChild(row);
+   })
 }
